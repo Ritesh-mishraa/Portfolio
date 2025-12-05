61c31699f4bcbaf3aa65ff7d385f82954c154157
Ritesh-mishraa <rajmi8948360380@gmail.com>
2025-12-06 01:10:26 +0530

    Added Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react"; 
+import Pre from "./Components/Pre";
 import Navbar from "./Components/Navbar"
 import About from "./Components/About"
 import Projects from "./Components/Project"
@@ -6,13 +8,24 @@ import Skills from './Components/Skills';
 import Contact from './Components/Contact';
 import ParticlesBackground from './Components/ParticlesBackground';
 import Home from './Components/Home';
+import "./App.css";
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1500); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      
-      <Navbar/>
+      <Pre load={load} />
       <ParticlesBackground />
+      <Navbar/>
       <Home />
       <About />
       <Skills />
